3e03f563b87311650c26c36eee3e2642d793b7e2
ramfridman <ramfridman1@gmail.com>
2022-05-27 17:58:24 +0300

    fixed Button color + fixed 100 on 100 grid

--- frontend/src/components/UI/Land.js
+++ frontend/src/components/UI/Land.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Button } from "react-bootstrap";
 import Popup from "reactjs-popup";
 import classes from "./Land.module.scss";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 const Land = (props) => {
   // const classesStr = `${classes["land"]} ${classes[props.type]} ${
   //   ["unowned", "owned", "owned-forSale"].includes(classes[props.type])
@@ -29,15 +31,17 @@ const Land = (props) => {
       {/* </Popup> */}
       <Button
         variant="primary"
+        className="rounded-0 p-0"
         style={{
           outline: "1px solid black",
           boxShadow: "none",
+          fontSize: "8px",
           width: "30px",
           height: "30px",
-        }}
+          }}
         id={props.id}
         key={props.id}
-      ></Button>
+      >{props.id}</Button>
     </>
   );
 };
