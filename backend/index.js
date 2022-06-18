042554e62d80a1907a1327e5925ab9adc6e326e3
ramfridman <ramfridman1@gmail.com>
2022-06-18 22:03:34 +0300

    added node js server with firebase access

--- backend/index.js
+++ backend/index.js
@@ -1,27 +1,35 @@
-import express from 'express';
-import * as admin from 'firebase-admin';
 
-var cors = require('cors')
-
-const bodyParser = require('body-parser')
+// import { createRequire } from "module";
+// const require = createRequire(import.meta.url);
+const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
+const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
+const express = require('express');
+var cors = require('cors');
+const bodyParser = require('body-parser');
 const serviceAccount = require('./config/firebase-config.json');
 
-const app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(cors())
 
-const port = process.env.PORT || 5002;
+const app = express();
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+// app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running on port ${port} 🔥`));
 
-const firebase = admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+initializeApp({
+  credential: cert(serviceAccount)
 });
-const firestore = firebase.firestore();
-const auth = firebase.auth()
-firestore.settings({ ignoreUndefinedProperties: true })
 
-app.use("/user", (<div>Hello user</div>))
-app.use("/mock", (<div>Hello mock</div>))
+const firestore = getFirestore();
 
-export { auth, firestore };
+//const auth = firebase.auth();
+//firestore.settings({ ignoreUndefinedProperties: true });
+
+// app.use("/user", ("<div>Hello user</div>"));
+// app.use("/mock", ("<div>Hello mock</div>"));
+
+//export { auth, firestore };
