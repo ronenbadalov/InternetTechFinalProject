55c57cd31a75127410d659a97ce5faa6c8904eb5
ramfridman <ramfridman1@gmail.com>
2022-07-09 13:58:36 +0300

    added all form validations for signup and login

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,4 +1,5 @@
 import React, { useContext, useEffect, useRef, useState } from "react";
+import {Link} from 'react-router-dom';
 import TextField from "@mui/material/TextField";
 import { Button } from "@mui/material";
 import { getUser, getUserFromSession } from "../helpers/userHelper";
@@ -6,16 +7,18 @@ import CurUserContext from "../store/curUser-context";
 const Login = () => {
   const curUserCtx = useContext(CurUserContext);
 
-  const [isUsernameError, setIsUsernameError] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(null);
+  const [isBtnClicked, setIsBtnClicked] = useState(false);
+  const [isEmailError, setIsEmailError] = useState(false);
   const [isPasswordError, setIsPasswordError] = useState(false);
   const [email, setEmail] = useState(null);
   const [password, setPassword] = useState(null);
 
-  const usernameValidityHandler = (e) => {
+  const emailValidityHandler = (e) => {
     if (e.target.value.length < 6) {
-      setIsUsernameError(true);
+      setIsEmailError(true);
     } else {
-      setIsUsernameError(false);
+      setIsEmailError(false);
     }
   };
   const passwordValidityHandler = (e) => {
@@ -28,15 +31,22 @@ const Login = () => {
 
   const submitFormHandler = async (e) => {
     e.preventDefault();
+    setIsBtnClicked(true);
     const user = await getUser(email, password);
-    curUserCtx.login(user);
+    if(user === undefined) {
+      setIsLoggedIn(false);
+    } else {
+      setIsLoggedIn(true);
+      curUserCtx.login(user);
+    }
   };
   return (
-    <div>
+    <div className="loginContainer">
       <form className="form" onSubmit={submitFormHandler}>
+      <h1 style={{textAlign: "center", fontSize: "40px"}}>Login</h1>
         <TextField
           // error={isUsernameError}
-          label="Username"
+          label="Email"
           variant="outlined"
           sx={{ margin: "0.3rem 0" }}
           // onBlur={usernameValidityHandler}
@@ -66,13 +76,24 @@ const Login = () => {
           size="small"
           required
         />
+        <div>
+          <Link to="/reset">Forgot Password?</Link>
+        </div>
+        {
+        (isBtnClicked && isLoggedIn === false) &&
+         (<h6 style={{color: "red", marginTop:"30px", marginLeft: "50px"}}>Incorrect email or password!</h6>)
+        }
         <Button
-          sx={{ margin: "auto", width: "150px" }}
+          sx={{ margin: "auto", width: "150px", marginTop:"30px" }}
           variant="contained"
           type="submit"
         >
           Login
         </Button>
+
+        <div style={{marginTop: "20px"}}>
+          Don't have an account? <Link to="/signup">Register</Link> now.
+        </div>
       </form>
     </div>
   );
--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -1,4 +1,5 @@
 import React, { useRef, useState } from "react";
+import {useNavigate, Link} from "react-router-dom";
 import TextField from "@mui/material/TextField";
 import {
   Button,
@@ -7,23 +8,38 @@ import {
   MenuItem,
   Select,
 } from "@mui/material";
-import { addUser } from "../helpers/userHelper";
+import { addUser, getUser } from "../helpers/userHelper";
+
+
 const Signup = () => {
+  const [isEmailError, setIsEmailError] = useState(false);
   const [isUsernameError, setIsUsernameError] = useState(false);
   const [isPasswordError, setIsPasswordError] = useState(false);
+  const [isSamePasswordError, setIsSamePasswordError] = useState(false);
   const [email, setEmail] = useState(null);
   const [name, setName] = useState(null);
   const [password, setPassword] = useState(null);
-  const [type, setType] = useState(null);
+  const [type, setType] = useState(0);
+  const navigate = useNavigate();
 
-  // add password confirmation
+  const emailValidityHandler = (e) => {
+    const emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    const emailValue = e.target.value;
+    if (emailValue.match(emailFormat)) {
+      setIsEmailError(false);
+    } else {
+      setIsEmailError(true);
+    }
+  };
+
   const usernameValidityHandler = (e) => {
-    if (e.target.value.length < 6) {
+    if (e.target.value.length < 4) {
       setIsUsernameError(true);
     } else {
       setIsUsernameError(false);
     }
   };
+
   const passwordValidityHandler = (e) => {
     if (e.target.value.length < 6) {
       setIsPasswordError(true);
@@ -32,44 +48,57 @@ const Signup = () => {
     }
   };
 
-  const submitFormHandler = (e) => {
+  const samePasswordValidityHandler = (e) => {
+    if (e.target.value !== password) {
+      setIsSamePasswordError(true);
+    } else {
+      setIsSamePasswordError(false);
+    }
+  };
+
+  const submitFormHandler = async (e) => {
     e.preventDefault();
-    addUser({ email, password, name, type });
-    console.log(email);
-    console.log(password);
-    console.log(name);
-    console.log(type);
+    if(!isEmailError &&
+      !isPasswordError &&
+      !isSamePasswordError &&
+      !isUsernameError) {
+        await addUser({ email, password, name, type });
+        await getUser(email, password);
+        navigate('/emailVerification');
+      }
   };
 
   return (
-    <div>
+    <div className="signupContainer">
       <form className="form" onSubmit={submitFormHandler}>
+      <h1 style={{textAlign: "center", fontSize: "40px"}}>Register</h1>
+
         <TextField
-          // error={isUsernameError}
+          error={isEmailError}
           label="Email"
           variant="outlined"
           sx={{ margin: "0.3rem 0" }}
-          // onBlur={usernameValidityHandler}
-          // helperText={
-          //   isUsernameError
-          //     ? `Username must contain more than 6 characters`
-          //     : ""
-          // }
+          onBlur={emailValidityHandler}
+          helperText={
+            isEmailError
+              ? `Not a Valid Email!`
+              : ""
+          }
           size="small"
           onChange={(e) => setEmail(e.target.value)}
           required
         />
         <TextField
-          // error={isUsernameError}
+          error={isUsernameError}
           label="Name"
           variant="outlined"
           sx={{ margin: "0.3rem 0" }}
-          // onBlur={usernameValidityHandler}
-          // helperText={
-          //   isUsernameError
-          //     ? `Username must contain more than 6 characters`
-          //     : ""
-          // }
+          onBlur={usernameValidityHandler}
+          helperText={
+            isUsernameError
+              ? `Name must contain more than 4 characters`
+              : ""
+          }
           size="small"
           onChange={(e) => setName(e.target.value)}
           required
@@ -91,6 +120,22 @@ const Signup = () => {
           size="small"
           required
         />
+         <TextField
+          error={isSamePasswordError}
+          label="Password Validator"
+          type="password"
+          autoComplete="current-password"
+          variant="outlined"
+          sx={{ margin: "0.5rem 0" }}
+          onBlur={samePasswordValidityHandler}
+          helperText={
+            isSamePasswordError
+              ? `Passwords don't match`
+              : ""
+          }
+          size="small"
+          required
+        />
         <FormControl>
           <InputLabel id="typeSelectLabel">Type</InputLabel>
           <Select
@@ -106,12 +151,15 @@ const Signup = () => {
           </Select>
         </FormControl>
         <Button
-          sx={{ margin: "auto", width: "150px" }}
+          sx={{ margin: "auto", width: "150px", marginTop: "30px" }}
           variant="contained"
           type="submit"
         >
           Sign Up
         </Button>
+        <div style={{marginTop: "20px"}}>
+          Already have an account? <Link to="/login">Login</Link> now.
+        </div>
       </form>
     </div>
   );
